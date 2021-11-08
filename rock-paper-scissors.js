8b7acb5eba67cbd60f6fa6e5810ceb6707b0b706
simeonbain <bainsimeon@gmail.com>
2021-11-09 01:53:08 +1100

    Implement playRound() function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,37 @@
 
+/* Plays a single round of Rock Paper Scissors */
+function playRound(playerSelection, computerSelection) {
+    // Parse the playerSelection into the expected format
+    playerSelection = capitalize(playerSelection);
+    
+    // Determine if the player won or not
+    let playerWon = false; 
+    switch (playerSelection) {
+        case `Rock`: 
+            if (computerSelection === `Scissors`) {
+                playerWon = true;  
+            }
+            break; 
+        case `Paper`:
+            if (computerSelection === `Rock`) {
+                playerWon = true;  
+            }
+            break; 
+        case `Scissors`:
+            if (computerSelection === `Paper`) {
+                playerWon = true;  
+            }
+            break; 
+    }
+    
+    // Based on the outcome, return the appropriate message
+    if (playerWon) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
 /* Randomly return either  `Rock`, `Paper` or `Scissors` */
 function computerPlay() {
     switch (getRandomInt(3)) {
@@ -16,4 +49,10 @@ function computerPlay() {
 /* Returns a random int in the range [0, max-1] */
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+
+/* Takes a string as input and returns the capitalized version of it 
+ * (first character uppercase, subsequent characters lowercase) */
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1).toLowerCase(); 
 }
