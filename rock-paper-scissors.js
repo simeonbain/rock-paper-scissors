75f25dc7d53cf3539dbd624b3c69b93dddb24f85
simeonbain <bainsimeon@gmail.com>
2021-11-14 02:17:26 +1100

    Display the running score, and announce a winner of the game once one player reaches 5 points

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,11 @@
+/* Global variables */ 
+let playerScore = 0; 
+let computerScore = 0; 
+const winningScore = 5; 
+
 /* Plays a game that keeps score and reports a winner or loser at 
  * the end */
-function game(playerSelection) {
+function playGame(playerSelection) {
 
     // Get the computer selection
     const computerSelection = computerPlay(); 
@@ -8,15 +13,29 @@ function game(playerSelection) {
     // Play a single round
     const result = playRound(playerSelection, computerSelection);
 
-    // Based on the result show the appropriate message
+    // Based on the result, increment the scores show the appropriate message
     if (result === `Draw`) {
-        showResult(`It's a draw! ${playerSelection} equals ${computerSelection}`);
+        showResult(`${playerSelection} equals ${computerSelection}`);
     } else if (result === `Player`) {
-        showResult(`You win! ${playerSelection} beats ${computerSelection}`);
+        showResult(`${playerSelection} beats ${computerSelection}`);
+        playerScore++; 
     } else if (result === `Computer`) { 
-        showResult(`You lose! ${computerSelection} beats ${playerSelection}`);
+        showResult(`${computerSelection} beats ${playerSelection}`);
+        computerScore++; 
     } else {
         showResult(`There was an error and the round could not be completed`);
+    }
+
+    // Show the score
+    showScore(playerScore, computerScore);
+
+    // End the game after winning score is reached
+    if (playerScore >= winningScore) {
+        endGame(); 
+    } else if (computerScore >= winningScore) {
+        endGame(); 
+    } else {
+        // keep playing
     }
 }
 
@@ -77,13 +96,53 @@ function showResult(result) {
     resultOutput.textContent = result; 
 } 
 
-/* Query selectors */
-const buttons = document.querySelectorAll(`button`); 
+/* Shows the score on the UI */ 
+function showScore(playerScore, computerScore) {
+    playerScoreOutput.textContent = `Your score: ${playerScore}`; 
+    computerScoreOutput.textContent = `Computer score: ${computerScore}`;
+}
+
+/* Shows the reset button on the UI and hides the selection buttons */
+function showReset() {
+    resetButton.hidden = false; 
+    selectionButtons.forEach((button) => button.hidden = true); 
+}
+
+/* Ends the game and shows the overall winner and a reset button on the UI */ 
+function endGame() {
+    if (playerScore > computerScore) {
+        showResult(`${resultOutput.textContent}. 
+            You won, you were the first to ${winningScore}!` );
+    } else if (computerScore > playerScore) {
+        showResult(`${resultOutput.textContent}. 
+            You lost, the computer was the first to ${winningScore}!`);
+    } else {
+        showResult(`${resultOutput.textContent}. It was a draw`); 
+    }
+
+    // show reset button 
+    showReset(); 
+}
+
+/* Resets the game to the initial state */ 
+function resetGame() {
+    playerScore = 0; 
+    computerScore = 0; 
+    showResult(``);
+    showScore(0, 0);
+    selectionButtons.forEach((button) => {button.hidden = false}); 
+    resetButton.hidden = true;
+}
+
+/* Elements */
+const selectionButtons = document.querySelectorAll(`.selection`); 
 const resultOutput = document.querySelector(`.result`);
+const playerScoreOutput = document.querySelector(`.playerScore`);
+const computerScoreOutput = document.querySelector(`.computerScore`);
+const resetButton = document.querySelector(`.reset`); 
 
 /* Event listeners */
-buttons.forEach((button) => {
-    button.addEventListener(`click`, () => { game(button.dataset.selection) });
+selectionButtons.forEach((button) => {
+    button.addEventListener(`click`, () => playGame(button.dataset.selection));
 });
-
-
+resetButton.addEventListener(`click`, () => resetGame());
